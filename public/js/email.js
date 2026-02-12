fac7db68aa2c52c9778e095fe3c56338289b7039
CamilleDelmas <kamilledelmas@gmail.com>
2026-02-12 15:05:57 +0100

    mise en forme foormulaire et interactivité

--- public/js/email.js
+++ public/js/email.js
@@ -1,15 +1,16 @@
 
 window.onload = function() {
             const form = document.getElementById('contact-form');
-            const thank = document.querySelector('.thank')
+            const thank = document.querySelector('p.hide')
 
             form.addEventListener('submit', function(event) {
                 event.preventDefault();
                 emailjs.sendForm('service_84alrqu', 'contact_form', this)
                     .then(() => {
                         console.log('SUCCESS!');
+                        form.classList.remove("show");
                         form.classList.add("hide");
-                        thank.classList.remove("thank")
+                        thank.classList.remove("hide")
                     }, (error) => {
                         console.log('FAILED...', error);
                     });
